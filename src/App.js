aa66543f5b07b64928bac872febbab9485f676be
Esther <esthc@outlook.es>
2022-08-14 19:44:09 +0200

    hola

--- src/App.js
+++ src/App.js
@@ -1,81 +1,82 @@
-import React from "react";
-import axios from "axios";
+import {useEffect, useState} from "react";
+import './App.css';
+import axios from 'axios';
 
-const CLIENT_ID = "2f1466b032bd43ba98236f5157ed4b1e";
-const REDIRECT_URI = "http://localhost:3000";
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const RESPONSE_TYPE = "token";
-const SCOPES = "user-top-read";
+function App() {
+    const CLIENT_ID = "2f1466b032bd43ba98236f5157ed4b1e"
+    const REDIRECT_URI = "https://spotify-d3ng1m67k-gitecv.vercel.app/"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+    const SCOPES = "user-top-read"
 
-export default class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = { token: "" };
-  }
+    const [token, setToken] = useState("")
+    const [artists, setArtists] = useState([])
 
-  getToken = () => {
-    debugger;
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    // Genera el token y lo almacena en el session storage
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
 
-    if (token && hash) {
-      token = hash
-        .substring(1)
-        .split("&")
-        .find((elem) => elem.startsWith("access_token"))
-        .split("=")[1];
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    // Permite eliminar el token y salir de la sesión
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
     }
 
-    this.setState(
-      {
-        token: token,
-      },
-      () => {
-        console.log(this.state.token);
-      }
-    );
-  };
+    const searchArtists = async (e) => {
+      debugger;
+      console.log(token)
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
 
-  searchArtists = async (e) => {
-    debugger;
-    e.preventDefault();
-    const { data } = await axios.get(
-      "https://api.spotify.com/v1/me/top/artists",
-      {
-        headers: {
-          Authorization: `Bearer ${this.state.token}`,
-        },
-      }
-    );
+        setArtists(data.items)
+    }
 
-    this.setState(
-      {
-        artist: data.items,
-      },
-      () => console.log(this.state.artist)
-    );
-  };
+    const renderArtists = () => {
+        return artists.map(artist => (
+            <div key={artist.name}>
+                {artist.name}
+            </div>
+        ))
+    }
 
-  render() {
     return (
-      <div>
-        <button onClick={this.getToken}>Ando despechá</button>
-        <header className="App-header">
-          <h1>Spotify React</h1>
-          <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`}
-          >
-            Login to Spotify
-          </a>
-          : <button>Logout</button>
-        </header>
-        <form onSubmit={this.searchArtists}>
-          <button type={"submit"}>Search</button>
-        </form>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <h1>Spotify React</h1>
+                {!token ?
+                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`}>Login
+                        to Spotify</a>
+                    : <button onClick={logout}>Logout</button>}
+
+                {token ?
+                    <form onSubmit={searchArtists}>
+                        <button type={"submit"}>Search</button>
+                    </form>
+
+                    : <h2>Please login</h2>
+                }
+
+                {renderArtists()}
+
+            </header>
+        </div>
     );
-  }
 }
+
+export default App;
